fd334b7fa9ae4141d36d2e5b7843e02bf57d145e
child9527 <31912488+child9527@users.noreply.github.com>
2026-09-07 09:11:34 +0800

    Update merge.py

--- scripts/merge.py
+++ scripts/merge.py
@@ -11,12 +11,17 @@ LOCAL_SOURCES = [
 ]
 
 # 2. 剩下的远程订阅源 (移除了本地已有的两个)
+from datetime import datetime
+# 自动生成当天日期
+today = datetime.now().strftime("%Y%m%d")
 REMOTE_SOURCES = [
+    f"https://free.datiya.com/uploads/{today}-clash.yaml",
     "https://raw.githubusercontent.com/snakem982/proxypool/main/source/clash-meta-2.yaml",
     "https://raw.githubusercontent.com/free18/v2ray/main/c.yaml",
     "https://raw.githubusercontent.com/mahdibland/V2RayAggregator/master/Eternity.yml",
     "https://github.com/PuddinCat/BestClash/raw/refs/heads/main/proxies.yaml"
 ]
+
 
 # 国家关键词与表情包映射
 COUNTRY_MAP = {
